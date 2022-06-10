eda45d622d34b3b69ea4f82ecb3d3d7844cc956a
rizkhal <lamaaurizkhal@gmail.com>
2022-06-10 18:12:58 +0900

    shopping

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\AuthJsonController;
+use App\Http\Controllers\ShoppingJsonController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthJsonController::class)->prefix('users')->group(function () {
     Route::get('/', 'users');
     Route::post('/signup', 'signup');
     Route::post('/signin', 'signin');
+});
+
+Route::controller(ShoppingJsonController::class)->middleware('auth:api')->prefix('shopping')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+    Route::get('/{shopping}', 'show');
 });
